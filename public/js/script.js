4e764b477b15b6ed081f1e2a7ab4c73be718e827
Artem Romanov <artdoua@gmail.com>
2017-06-04 01:35:03 +0300

    in progress

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,36 @@
 /**
  * Created by User on 13.05.2017.
  */
-var doc = document.querySelector('.close');
-function hendler(event){
-  // console.log(event.code);
-    if(event.code=='KeyV'){
-        doc.className = 'hidden';
-    }else{
-        doc.className = 'close';
+(function () {
+    var doc = document.querySelector('.close');
+
+    function hendler(event) {
+         console.log(event.keyCode);
+        if (event.keyCode === 86) {
+            doc.className = 'hidden';
+            $('.promo-img__text-item').each(function(){
+                $(this).hide();
+                $(this).animate({
+                        opacity: 'show',
+                        left: "+=500",
+                        //height: "toggle"
+                    }, 3000, function() {
+                        // Animation complete.
+                    });
+                });
+        } else {
+            doc.className = 'close';
+            $('.promo-img__text-item').each(function(){
+                $(this).css({
+                    left: "-500px"
+                });
+            });
+        }
+
     }
+    window.addEventListener('keyup', hendler);
+//    $('.close').on("dblclick swipe", function(){
+//        hendler({keyCode: 86});
+//    });
 
-}
-
-
-
-window.addEventListener('keypress', hendler);
+})();
